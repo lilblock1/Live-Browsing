c449e41b11d23b3f5b9b982c2d3e2e5588f3aa40
lilblock1 <58580672+lilblock1@users.noreply.github.com>
2022-04-17 14:27:35 -0500

    not done yet

---
+++ script2.js
@@ -0,0 +1,6 @@
+function submit() {
+    document.getElementById('content').innerHTML = '';
+    var val = document.getElementById('search').value;
+    var newElement = document.createElement('script');
+    newElement.scr = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyBgqTpHgS_akhX0-des2nGWBX42eiBoWsA&cx='
+}
