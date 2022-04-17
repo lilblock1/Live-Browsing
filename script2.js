2ef5add9af83032f1259d9441bc1733e6c83310b
lilblock1 <58580672+lilblock1@users.noreply.github.com>
2022-04-17 17:07:59 -0500

    YES

--- script2.js
+++ script2.js
@@ -1,8 +1,24 @@
 function submit() {
-    document.getElementById('content').innerHTML = '';
-    var val = document.getElementById('search').value;
-    var newElement = document.createElement('script');
-    newElement.scr = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyBgqTpHgS_akhX0-des2nGWBX42eiBoWsA&cx=938838ca299c6a840&q=${val}&callback=hndlr';
-    newElement.id = "mainscript";
-    document.body.appendChild(newElement);
+  document.getElementById('buttons').style.display = 'block';
+  document.getElementById('content').innerHTML = '';
+  var val = document.getElementById('search').value;
+  var newelement = document.createElement('script');
+  newelement.src = `https://www.googleapis.com/customsearch/v1?key=AIzaSyB06Unrbi3Dh3LH-Hbgsdm7qd9HIDWj7gE&cx=partner-pub-6291336405621919:2662881632&q=${val}&callback=hndlr`;
+  newelement.id = 'mainscript';
+  document.body.appendChild(newelement);
+}
+
+async function submitImages() {
+  document.getElementById('content').innerHTML = '';
+  var val = document.getElementById('search').value;
+  var clientId = "4N8myJtDKWa_ka7K1sqV1Y5pcRXsDjJqL5J-tx0nTtk";
+  var url = `https://api.unsplash.com/search/photos/?client_id=${clientId}&query=${val}&per_page=30`;
+
+  var promise = await fetch(url);
+  var data = await promise.json();
+  
+  data.results.forEach(function(photo) {
+    var result = `<img src=${photo.urls.regular} style="width: 200px; height: 133px; margin: 10px; vertical-align: top; object-fit: cover;">`;
+    document.getElementById('content').innerHTML += result;
+  })
 }
